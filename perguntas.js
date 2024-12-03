41e50789eac42616ac6d42128ad47a1c59ac8562
nika-joyboy <00001108115251sp@al.educacao.sp.gov.br>
2024-12-03 20:21:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'fisica',
+    'O que é fisica?',
+    'O estudo do universo'
+)
+
+criaCartao(
+    'Geografia',
+    'onde nasceu albert einstein',
+    'Ulm, Alemanha'
+)
+
+criaCartao(
+    'fisica',
+    'O que é uma estrela?',
+    'As estrelas são grandes esferas formadas por plasma aquecido a milhares de graus. Seu formato deve-se à sua gravidade, que aponta em direção ao núcleo da estrela.'
+)
+
+criaCartao(
+    'Lingua de albert einstein',
+    'Como se diz oi em alemao?',
+    'Oi em alemao é Hey (RAI)'
+)
